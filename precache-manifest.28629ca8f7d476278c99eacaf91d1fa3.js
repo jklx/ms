5aaaaca22d0861b8a79fb5c135d848a8d0305d3f
Jakob Waldmüller <jakob.waldmueller@cisha.de>
2020-01-18 21:22:55 +0100

    Updates

---
+++ precache-manifest.28629ca8f7d476278c99eacaf91d1fa3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafceebb4438fc95691be9c82d9fe600",
+    "url": "/ms/index.html"
+  },
+  {
+    "revision": "3d86ab9f524508d4e907",
+    "url": "/ms/static/css/main.668fa4dc.chunk.css"
+  },
+  {
+    "revision": "c5b8a6cc1ccc769fde0c",
+    "url": "/ms/static/js/2.f28a06b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ms/static/js/2.f28a06b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d86ab9f524508d4e907",
+    "url": "/ms/static/js/main.327ea2ac.chunk.js"
+  },
+  {
+    "revision": "979a8614cdac8377e5bc",
+    "url": "/ms/static/js/runtime-main.a29bc591.js"
+  },
+  {
+    "revision": "7d33396df6dbbb3d251d07d2643fed9d",
+    "url": "/ms/static/media/Efeld.7d33396d.svg"
+  },
+  {
+    "revision": "033546f4738e1f68aa3a902ab6d4e51d",
+    "url": "/ms/static/media/Magnetfeld.033546f4.svg"
+  },
+  {
+    "revision": "8349afd12f0dd8b7e659137d88a8d796",
+    "url": "/ms/static/media/Skala.8349afd1.svg"
+  }
+]);
